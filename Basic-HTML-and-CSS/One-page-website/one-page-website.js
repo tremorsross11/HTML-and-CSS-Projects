8470ae751824a6047a7715c446397880d890c5be
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2022-12-06 17:27:28 -0800

    bug fixes

--- Basic-HTML-and-CSS/One-page-website/one-page-website.js
+++ Basic-HTML-and-CSS/One-page-website/one-page-website.js
@@ -27,7 +27,7 @@ function openModal() {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+    for (i = 1; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
